bdc386be05d31d349eae4b5c97d82a1ab00155df
VotreNom <votre.email@exemple.com>
2026-09-21 06:41:01 +0200

    fix page file name properly

--- app/account/orders/page (2).tsx
+++
@@ -1,3 +0,0 @@
-export default function OrdersPage() {
-  return <div>Mes commandes</div>
-}
--- app/account/orders/page.tsx
+++ app/account/orders/page.tsx
@@ -1,1 +1,3 @@
-export default function OrdersPage() { return <div>Mes commandes</div> }
+export default function OrdersPage() {
+  return <div>Mes commandes</div>
+}
